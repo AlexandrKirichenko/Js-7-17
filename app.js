7bc3715460527eca414a53720950aa9f21e34cd4
AlexandrKirichenko <52669128+AlexandrKirichenko@users.noreply.github.com>
2019-07-17 17:16:31 +0300

    js
    js

---
+++ app.js
@@ -0,0 +1,122 @@
+// var apple = 12;
+// const con = 123;
+// let a = 0; 
+// let b = 0;
+// console.log(a++);
+// console.log(++b);
+// console.log(72 >= Number('G'));
+// var years = prompt('Сколько вам лет?',11);
+// alert('Вам' + years +'лет');
+// Имя?Сколько лет? Привет тебе столько-то лет.
+// alert('name?');
+// let years = Number(prompt('сколько вам лет?'));
+// alert("Привет тебе" + years + "лет")
+// const name = prompt("Name");
+// const age = prompt("Age");
+// alert(name +" "+ age);
+// ili
+// let result ='';
+// const name = prompt("Name");
+// const age = prompt("Age");
+// result = name +" "+ age;
+// alert(result);
+// ili 
+// let result ='';
+// result = prompt("Name") +", "+ prompt("Age");
+// alert(result);
+// alert(prompt("Name") +", "+ prompt("Age"));
+// // ili
+// const name = prompt("Name");
+// const age = prompt("Age");
+// alert(`Your name ${name}. Your age ${age}`);
+// if(2>1){
+//     console.log("!!!");
+// }else{
+//     console.log("---");
+// }
+// let login='';
+// let password='';
+// do{
+
+// } 
+
+// for(let i = 0; i < 10; i += 2){
+//     console.log(i);
+// }
+// var hello ='Hello Wold!';
+// var massage;
+// massage = hello;
+// alert( hello );
+// alert( massage);
+// var name = "Nik";
+// console.log("Hi" + name);
+// if (name.length > 3) {
+//     console.log("to long name");
+// } else if(name.length == 3) {console.log("norm");}
+// else {console.log("short");}
+// var ovec = 0;
+// while (ovec < 20) {
+//     console.log(''+ ovec);ovec++;
+// }
+// console.log('sleep');
+// for (var ovec = 0;
+//     // (настройка- этот код будет вып до начала цикла)
+//     ovec < 100;
+//     // условие true или false
+//     ovec++
+//     // код который будет выполнятся после каждого повтра цикла
+//     ) {console.log(''+ ovec);}
+//     // код который будет выполнятся, до тех пор пока условие дает true;
+//     console.log('sleep');
+    // var timehello = 3;
+    // for (var i=0;i < timehello; i++) {console.log("hi");}
+
+
+
+
+
+
+
+
+
+
+
+//     Настройка (var sheepCounted = 0) выполняется до запуска цикла.
+// Как правило, здесь создают переменную для отслеживания количества
+// повторов. В нашем случае это переменная sheepCounted с начальным
+// значением 0.
+// Условие (sheepCounted < 10) проверяется перед каждым повтором тела цикла. Если условие дает true, тело выполняется, иначе цикл
+// заканчивает работу. В нашем случае цикл остановится, когда значение
+// sheepCounted достигнет 10.
+// Приращение (sheepCounted++) выполняется после каждого
+// повтора тела цикла. Как правило, здесь изменяют значение переменной цикла. В этом примере мы после каждого повтора увеличиваем
+// sheepCounted на 1.
+//   i - счетчик
+// var animals =["lev","lvica"];
+// for (i = 0;i < animals.length;i++) {
+//     console.log("tupo vse " + animals[i]);
+// }
+
+
+
+// var animals =["lev","medved"];
+    // for (var i = 0;i < animals.length; i++) {
+    //     console.log("В этом зоопарке есть " + animals[i]);
+    // }
+
+for (let i= 0;i < 100;i += 2) {
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
